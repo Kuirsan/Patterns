ec7db552d610c2ad69462ddbed986eec15a02bc5
Кирилл Павлов <ksmersh@gmail.com>
2020-09-03 22:06:40 +0300

    SingletonPattern with 2 check pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton singletonInstance;
+        private static readonly object padlock = new object();
+
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            if (singletonInstance == null)
+            {
+                lock (padlock)
+                {
+                    if (singletonInstance == null)
+                    {
+                        singletonInstance = new Singleton();
+                    }
+                }
+            }
+            return singletonInstance;
+        }
+    }
+}
